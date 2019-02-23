a61db4bbf32dd00b00c5a084ff3ea655b44eb633
Carlos Ordonez <44660718+Carlos-Ordonez@users.noreply.github.com>
2019-02-22 22:40:41 -0500

    Update k_MeansFunction.R

--- k_MeansFunction.R
+++ k_MeansFunction.R
@@ -1,5 +1,8 @@
 my_K_Means<-function(myData,k){
   
+  pause_time<-.5
+  start_time<-Sys.time()
+  
   #Create list to hold component dataframes
   components<-list()
   for (i in 1:ncol(myData)){
@@ -23,9 +26,9 @@ my_K_Means<-function(myData,k){
   
   #create list of clusters dataframes
   clusters<-list()
-    for(i in 1:k){
-      clusters[[i]]<-centroids[i,]
-    }
+  for(i in 1:k){
+    clusters[[i]]<-centroids[i,]
+  }
   
   #Calculate the distance from each point to the centroids
   #Returns a list of distances
@@ -44,24 +47,65 @@ my_K_Means<-function(myData,k){
   
   #Classify points in to respective clusters 
   #using the distance list to index which cluster to classify as
-  for(j in (k+1):length(randomIndex)){
-    distances<-distance(centroids,myDataComponents,randomIndex[j])
-    selected_Point<-c(myDataComponents[[1]][randomIndex[j],])
-    
-    for (s in 2:length(myDataComponents)){
-      selected_Point[s]<-myDataComponents[[s]][randomIndex[j],]
+  new_centroids<-centroids
+  current_centroids<-data.frame()
+  itterations<-0
+  while(isTRUE(all.equal(new_centroids,current_centroids))==FALSE){
+    itterations<-itterations +1
+    centroids<-new_centroids
+    clusters<-list()
+    for(i in 1:k){
+      clusters[[i]]<-new_centroids[i,]
     }
-    clusters[[which.min(distances)]]<-rbind(clusters[[which.min(distances)]],selected_Point)
+    for(j in (k+1):length(randomIndex)){
+      distances<-distance(centroids,myDataComponents,randomIndex[j])
+      
+      selected_Point<-c(myDataComponents[[1]][randomIndex[j],])
+      for (s in 2:length(myDataComponents)){
+        selected_Point[s]<-myDataComponents[[s]][randomIndex[j],]
+      }
+      
+      clusters[[which.min(distances)]]<-rbind(clusters[[which.min(distances)]],selected_Point)
+    }
+    current_centroids<-centroids
+    for (i in 1:nrow(centroids)){
+      for (j in 1:length(components)){
+        new_centroids[i,j]<-sum(clusters[[i]][,j]/length(clusters[[i]][,j]))
+      }
+    }
+    print(new_centroids)
+    for (i in 1:k){
+      clusters[[i]]$CID<-i
+    }
+    cluster_Itteration<-as.data.frame(rbindlist(clusters))
+    plot(cluster_Itteration[,1]~cluster_Itteration[,2], col=cluster_Itteration$CID + 3, cex = 1.5, pch = 20)
+    
+    for (i in 1:nrow(centroids)){
+      points(centroids[i,2],centroids[i,1],col = "red", cex = 3,pch = 10)
+    }
+    
+    Sys.sleep(pause_time)
+    
   }
-  for (i in 1:k){
-    clusters[[i]]$CID<-i
-  }
+  print(paste("Itterations:", itterations))
+  end_time<-Sys.time()
+  print((end_time-start_time)-(itterations*pause_time))
+  
   return(as.data.frame(rbindlist(clusters)))
 }
 
-Final_Cluster<-my_K_Means(myData,2)
+Final_Cluster<-my_K_Means(myData,3)
 
-plot(Final_Cluster[,1]~Final_Cluster[,2], col=Final_Cluster$CID + 3, cex = 1, pch = 19)
 
+Centroids
 Final_Cluster
 str(Final_Cluster)
+
+
+
+
+
+
+
+
+
